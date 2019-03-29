78dfb1628f3278f3382d3f3aa8a3b6e8d425d893
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-03-28 21:54:50 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dead by Daylight: Killer Randomizer
+Just a small little tkinter app that rolls 6 random killers :)
+
+### Upcoming Features (◠‿◠✿)
+
+GUI Based slider to choose how many killers rolled
+Single Killer Mode with Perk Randomizer
---
+++ killerssimple.py
@@ -0,0 +1,55 @@
+import tkinter as Tk
+from random import *
+
+frame = Tk.Tk()
+
+lbl = Tk.Label(
+    frame,
+    text=""
+)
+lbl.pack()
+
+
+def roll6():
+
+    # Dictionary of killers, to add a future killer simply iterate up by one and associate that number to the new killers name
+
+    killers = {
+        1: "Trapper",
+        2: "Wraith",
+        3: "Hillbilly",
+        4: "Nurse",
+        5: "Huntress",
+        6: "Legion",
+        7: "Michael",
+        8: "Hag",
+        9: "Cannibal",
+        10: "Doctor",
+        11: "Freddy",
+        12: "Pig",
+        13: "Clown",
+        14: "Spirit"
+        15: "Plague"
+    }
+
+    # Make sure to change the max randint (currently set to 15) to the highest number in the dictionary!
+    # Currently set to give you 6 random names, you can change the value located inside of -> Range() if you want more or less
+
+    x = randint(1, 15)
+    y = []
+    for i in range(6):
+        y.append(killers.get(x))
+        x = randint(1, 15)
+    lbl.config(text=y)
+
+
+btn = Tk.Button(
+    frame,
+    text="Roll 6 killers",
+    command=roll6,
+    height=80,
+    width=80
+).pack()
+
+frame.geometry("350x100")
+frame.mainloop()
